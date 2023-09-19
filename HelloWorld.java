d382ab45049db2174275ccfd72060a47edc56c03
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-09-19 11:24:21 -0700

    whilecode from assignment apcsp

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,24 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-            int i;   
-            float f = 2.5f;
-            boolean flag = true;
+          int a = 5;
+        int b = 2;
+        int c = 1;
+        int d = -8;
+        int i = 0;
+        while(i < 15){
+            System.out.println("i is less than 15");
+            i = i + 1;
+        }
+        int k = 6;
+        while (k > 5){
+            System.out.println("k is greater than 5");
+            k = k -1;
+        }
+        while (a < 10 & b <= 20){
+            System.out.println("both conditions have been met");
+            a = a + 1;
+        }
     }
 }
 
